7e520d4f05bf9550ad94c38e4ea9c328f228afdf
Kodai Yoneyama <electrical.joker.0315@gmail.com>
2021-12-06 12:49:42 +0900

    modify count provider

--- lib/data/todo_list.dart
+++ lib/data/todo_list.dart
@@ -53,21 +53,21 @@ class TodoList extends StateNotifier<List<Todo>> {
     if(id=='1'){
       ref.read(iconCount1Provider.state).state+=3;
       final iconNumber1=ref.read(iconCount1Provider);
-      if(iconNumber1==10){
+      if(iconNumber1==13){
         ref.read(iconCount1Provider.state).state=1;
       }
     }
     if(id=='2'){
       ref.read(iconCount2Provider.state).state+=3;
       final iconNumber2=ref.read(iconCount2Provider);
-      if(iconNumber2==11){
+      if(iconNumber2==14){
         ref.read(iconCount2Provider.state).state=2;
       }
     }
     if(id=='3'){
       ref.read(iconCount3Provider.state).state+=3;
       final iconNumber3=ref.read(iconCount3Provider);
-      if(iconNumber3==12){
+      if(iconNumber3==15){
         ref.read(iconCount3Provider.state).state=3;
       }
     }
